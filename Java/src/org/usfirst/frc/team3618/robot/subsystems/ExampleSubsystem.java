b26f538aa41976c6e0fc88e9e6f904c57a928af7
JoshuaJacobson <fudbuds@gmail.com>
2016-12-14 09:44:11 -0500

    Session 1 Code

---
+++ Java/src/org/usfirst/frc/team3618/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,28 @@
+
+package org.usfirst.frc.team3618.robot.subsystems;
+
+import org.usfirst.frc.team3618.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ExampleSubsystem extends Subsystem {
+	
+	static Talon left = new Talon(RobotMap.leftMotor);
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public static void driveRobot(double y) {
+    	left.set(y);
+    }
+}
+
