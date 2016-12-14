9cb87a209d56d69187c404e59460c1e8cba0051b
JoshuaJacobson <fudbuds@gmail.com>
2016-12-14 09:44:38 -0500

    Add files via upload

---
+++ Josh/src/org/usfirst/frc/team3618/robot/subsystems/ExampleSubsystem.java
@@ -0,0 +1,29 @@
+
+package org.usfirst.frc.team3618.robot.subsystems;
+
+import org.usfirst.frc.team3618.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class ExampleSubsystem extends Subsystem {
+ 
+	static Victor left = new Victor(RobotMap.leftMotorController);
+	
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+    public static void forwardDrive(double speed) {
+    left.set(speed);	
+    }
+
+}
+
